833d11abf264ef4aa70cd0614f46af7c0bdb11df
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-17 00:07:26 -0400

    Added R

---
+++ R/pdf.R
@@ -0,0 +1,5 @@
+pdf <- function(..., latex_engine = "xelatex", page_total = FALSE,
+                      show_footer = TRUE) {
+  template <- system.file("rmarkdown", "templates", "awesomecv"
+                          package = "amaRyaml"
+  )
