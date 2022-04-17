c04706b81551a211c4b9e2f568898cc3cd41949d
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-17 00:13:25 -0400

    fixed pdf.R

--- R/pdf.R
+++ R/pdf.R
@@ -1,5 +1,1 @@
-pdf <- function(..., latex_engine = "xelatex", page_total = FALSE,
-                      show_footer = TRUE) {
-  template <- system.file("rmarkdown", "templates", "awesomecv",
-                          package = "amaRyaml"
-  )
+pdf <- function(...) {template <- system.file("rmarkdown", "templates", "awesomecv", package = "vitae")}
