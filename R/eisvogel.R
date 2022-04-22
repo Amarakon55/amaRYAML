cc5b2cc30447fc21c354890cca498ca9f50f9663
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-22 16:45:08 -0400

    changed cv_document to pdf_document

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -6,6 +6,6 @@ eisvogel <- function(..., number_sections = TRUE, toc_depth = 3, latex_engine = 
   #pandoc_vars <- list()
   #if(page_total) pandoc_vars$page_total <- TRUE
   #if(show_footer) pandoc_vars$show_footer <- TRUE
-  cv_document(...,
+  pdf_document(...,
               template = template, number_sections = number_sections, toc_depth = toc_depth, latex_engine = latex_engine)
 }
