8e9dbe62b504eadd9ef00e1b4944ca1894c05c91
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-22 01:55:44 -0400

    Wrapped xelatex in quotes

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -1,6 +1,6 @@
 eisvogel <- function(..., number_sections = TRUE,
                           toc_depth = 3,
-			  latex_engine = xelatex) {
+			  latex_engine = "xelatex") {
   pdf_document_format("eisvogel",
     number_sections = number_sections, toc_depth = toc_depth, latex_engine = latex_engine, ...
   )
