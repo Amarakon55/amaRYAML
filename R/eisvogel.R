79325fd639350c1094e7fdca796d4d6ca1d858c8
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-23 18:30:40 -0400

    added comma

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -3,7 +3,7 @@ eisvogel <- function(..., number_sections = TRUE,
 			  latex_engine = "xelatex",
 			  template = "eisvogel") {
 			  #includes = includes(in_header = "/resources/eisvogel-in_header.tex")) {
-  pdf_document_format("eisvogel"
+  pdf_document_format("eisvogel",
     number_sections = number_sections,
     toc_depth = toc_depth,
     latex_engine = latex_engine,
