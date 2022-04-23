c7d6ec0b378232ddd54926ce91ce51c16723bbbc
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-23 18:58:35 -0400

    added in_heuader

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -1,8 +1,12 @@
+in_header <- system.file("rmarkdown", "templates", "eisvogel",
+                        "resources", "in_header.tex",
+                        package = "amaRyaml")
+
 eisvogel <- function(..., number_sections = TRUE,
 		          toc_depth = 3,
 			  latex_engine = "xelatex",
 			  template = "eisvogel",
-			  includes = includes(in_header = find_resource(format, "eisvogel-in_header.tex"))) {
+			  includes = includes(in_header = in_header)) {
   bookdown::pdf_document2(
     number_sections = number_sections,
     toc_depth = toc_depth,
