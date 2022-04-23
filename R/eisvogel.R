2ce71d6e449dfebabcd3c9d960b96aa53c2f3ea8
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-23 18:42:45 -0400

    added table-use-row-colors

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -1,15 +1,13 @@
 eisvogel <- function(..., number_sections = TRUE,
 		          toc_depth = 3,
 			  latex_engine = "xelatex",
-			  template = "eisvogel",
-			  table-use-row-colors = TRUE) {
+			  template = template("eisvogel", table-use-row-colors = TRUE)) {
 			  #includes = includes(in_header = "/resources/eisvogel-in_header.tex")) {
   bookdown::pdf_document2(
     number_sections = number_sections,
     toc_depth = toc_depth,
     latex_engine = latex_engine,
-    template = template,
-    table-use-row-colors = table-use-row-colors, ...
+    template = template, ...
     #includes = includes, ...
   )
 }
