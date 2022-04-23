c2a93d07748efdec7526d38c4d52c3b6cb5c0ca3
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-23 19:05:12 -0400

    added header

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -1,14 +1,12 @@
-header <- function (...) {
-    in_header <- system.file("rmarkdown", "templates", "eisvogel",
-                        "resources", "in_header.tex",
-                        package = "amaRyaml")
-}
 
 eisvogel <- function(..., number_sections = TRUE,
 		          toc_depth = 3,
 			  latex_engine = "xelatex",
 			  template = "eisvogel",
-			  includes = includes(in_header = header)) {
+			  includes = includes(in_header = in_header)) {
+  in_header <- system.file("rmarkdown", "templates", "eisvogel",
+                           "resources", "in_header.tex",
+                           package = "amaRyaml")
   bookdown::pdf_document2(
     number_sections = number_sections,
     toc_depth = toc_depth,
