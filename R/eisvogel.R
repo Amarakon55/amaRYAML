9e5854b76f1fa5d618b983d1541bdf8ed5d79d39
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-23 18:07:15 -0400

    added eisvogel.R

---
+++ R/eisvogel.R
@@ -0,0 +1,12 @@
+eisvogel <- function(..., number_sections = TRUE,
+		          toc_depth = 3,
+			  latex_engine = "xelatex",
+			  template = "eisvogel",
+			  includes(in_header = "resources/eisvogel-in_header.tex")) {
+  pdf_document_format("eisvogel",
+    number_sections = number_sections,
+    toc_depth = toc_depth
+    latex_engine = latex_engine,
+    template = template, ...
+  )
+}
