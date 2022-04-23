4aa14da2ba5587ea2daacb1a0a927761aa921237
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-23 18:51:58 -0400

    added find_resources

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -1,13 +1,13 @@
 eisvogel <- function(..., number_sections = TRUE,
 		          toc_depth = 3,
 			  latex_engine = "xelatex",
-			  template = template("eisvogel", table-use-row-colors = TRUE)) {
-			  #includes = includes(in_header = "/resources/eisvogel-in_header.tex")) {
+			  template = "eisvogel",
+			  includes = includes(in_header = find_resource(format, "eisvogel-in_header.tex"))) {
   bookdown::pdf_document2(
     number_sections = number_sections,
     toc_depth = toc_depth,
     latex_engine = latex_engine,
-    template = template, ...
-    #includes = includes, ...
+    template = template,
+    includes = includes, ...
   )
 }
