e2d140a3023e61347c583f372fce2c41961304df
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-22 21:25:45 -0400

    removed R

--- R/eisvogel.R
+++
@@ -1,7 +0,0 @@
-eisvogel <- function(..., number_sections = TRUE,
-                          toc_depth = 3,
-			  latex_engine = "xelatex") {
-  pdf_document_format("eisvogel",
-    number_sections = number_sections, toc_depth = toc_depth, latex_engine = latex_engine, ...
-  )
-}
