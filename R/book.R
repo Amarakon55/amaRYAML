1068010a7c19a75335550ff760b492bd95b09b71
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-24 18:03:58 -0400

    added indent

--- R/book.R
+++ R/book.R
@@ -1,12 +1,13 @@
 book <- function(..., toc_depth = 3,
-		 latex_engine = "xelatex",
-		 header_includes = "\\setlength\\parindent{24pt}") {
+		 latex_engine = "xelatex") {
   in_header <- system.file("includes", "in_header.tex",
                            package = "amaRyaml")
+  indent <- system.file("includes", "indent.tex",
+			package = "amaRyaml")
   bookdown::pdf_book(
     toc_depth = toc_depth,
     latex_engine = latex_engine,
-    includes = includes(in_header = in_header),
-    header_includes = header_includes, ...
+    includes = [includes(in_header = in_header),
+		includes(in_header = indent)], ...
   )
 }
