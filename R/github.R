d9cf438e1694ac08c46cc245a3836b0308f92e43
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-24 19:46:43 -0400

    simplified it

--- R/github.R
+++ R/github.R
@@ -1,9 +1,1 @@
-github <- function(..., number_sections = TRUE,
-			toc = TRUE,
-			toc_depth = 3) {
-  bookdown::github_document2(
-    number_sections = number_sections,
-    toc = toc,
-    toc_depth = toc_depth, ...
-  )
-}
+github <- function(...) { bookdown::github_document2(...) }
