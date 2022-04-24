67bb7b041db6345f06909e3c56164bfa47719654
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-23 22:50:20 -0400

    removed header file

--- R/github.R
+++ R/github.R
@@ -2,13 +2,10 @@ github <- function(..., html_preview = FALSE,
 		        number_sections = TRUE,
 			toc = TRUE,
 			toc_depth = 3) {
-  in_header <- system.file("includes", "in_header.tex",
-                           package = "amaRyaml")
   bookdown::github_document2(
     html_preview = html_preview,
     number_sections = number_sections,
     toc = toc,
-    toc_depth = toc_depth,
-    includes = includes(in_header = in_header), ...
+    toc_depth = toc_depth, ...
   )
 }
