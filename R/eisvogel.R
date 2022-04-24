716bddbae1d01617f4cc491d0cb2a04705cdd19e
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-23 21:43:58 -0400

    uncommented

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -1,16 +1,15 @@
-
 eisvogel <- function(..., number_sections = TRUE,
 		          toc_depth = 3,
 			  latex_engine = "xelatex",
 			  template = "eisvogel") {
-  #in_header <- system.file("rmarkdown", "templates", "eisvogel",
-  #                         "resources", "in_header.tex",
-  #                         package = "amaRyaml")
+  in_header <- system.file("rmarkdown", "templates", "eisvogel",
+                           "resources", "in_header.tex",
+                           package = "amaRyaml")
   bookdown::pdf_document2(
     number_sections = number_sections,
     toc_depth = toc_depth,
     latex_engine = latex_engine,
     template = template, ...
-    #includes = includes(in_header = in_header), ...
+    includes = includes(in_header = in_header), ...
   )
 }
