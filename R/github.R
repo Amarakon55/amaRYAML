037ec9c5310f39c78c3da9c87de9b3e5d94d4adf
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-24 19:48:54 -0400

    set number_sections to FALSE

--- R/github.R
+++ R/github.R
@@ -1,1 +1,2 @@
-github <- function(...) { bookdown::github_document2(...) }
+github <- function(..., number_sections = FALSE) {
+    bookdown::github_document2(number_sections = number_sections, ...) }
