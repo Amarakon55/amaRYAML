9b5c2e140efafcd48e11cf5aa0bc94f53455c28b
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-24 18:39:04 -0400

    removed html_preview option

--- R/github.R
+++ R/github.R
@@ -1,9 +1,7 @@
-github <- function(..., html_preview = FALSE,
-		        number_sections = TRUE,
+github <- function(..., number_sections = TRUE,
 			toc = TRUE,
 			toc_depth = 3) {
   bookdown::github_document2(
-    html_preview = html_preview,
     number_sections = number_sections,
     toc = toc,
     toc_depth = toc_depth, ...
