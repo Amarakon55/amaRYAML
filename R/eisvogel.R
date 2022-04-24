314e1f3be97124432eee2d317b4a8fbc5806c32a
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-23 22:29:14 -0400

    removed bookdown::

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -4,7 +4,7 @@ eisvogel <- function(..., number_sections = TRUE,
 			  template = "eisvogel") {
   in_header <- system.file("includes", "in_header.tex",
                            package = "amaRyaml")
-  bookdown::pdf_document2(
+  pdf_document2(
     number_sections = number_sections,
     toc_depth = toc_depth,
     latex_engine = latex_engine,
