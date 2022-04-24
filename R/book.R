7e668b3b23655847c388fa003800693702e68537
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-24 17:55:41 -0400

    added header_includes

--- R/book.R
+++ R/book.R
@@ -1,9 +1,12 @@
-book <- function(..., toc_depth = 3, latex_engine = "xelatex") {
+book <- function(..., toc_depth = 3,
+		 latex_engine = "xelatex",
+		 header_includes = "\\setlength\\parindent{24pt}") {
   in_header <- system.file("includes", "in_header.tex",
                            package = "amaRyaml")
   bookdown::pdf_book(
     toc_depth = toc_depth,
     latex_engine = latex_engine,
-    includes = "\\setlength\\parindent{24pt}" includes(in_header = in_header), ...
+    includes = includes(in_header = in_header),
+    header_includes = header_includes, ...
   )
 }
