a8e4e7ecaab2af2b336d3ae43b6b6ce587c483c1
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-23 22:17:00 -0400

    added book.R

---
+++ R/book.R
@@ -0,0 +1,10 @@
+eisvogel <- function(..., toc_depth = 3,
+			  latex_engine = "xelatex") {
+  in_header <- system.file("includes", "in_header.tex",
+                           package = "amaRyaml")
+  bookdown::pdf_book(
+    toc_depth = toc_depth,
+    latex_engine = latex_engine,
+    includes = includes(in_header = in_header), ...
+  )
+}
