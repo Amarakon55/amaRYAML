f8bfcedc813ec74673aca8d8820c7c27cf8aba12
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-29 00:42:16 -0400

    set toc to false

--- R/book.R
+++ R/book.R
@@ -1,10 +1,12 @@
-book <- function(..., toc_depth = 3,
+book <- function(..., toc = FALSE,
+		      toc_depth = 3,
 		      latex_engine = "xelatex") {
   in_header <- system.file("includes", "in_header.tex",
                            package = "amaryaml")
   indent <- system.file("includes", "indent.tex",
 			package = "amaryaml")
   bookdown::pdf_book(
+    toc = toc,
     toc_depth = toc_depth,
     latex_engine = latex_engine,
     includes = includes(in_header = in_header, before_body = indent), ...
--- R/eisvogel.R
+++ R/eisvogel.R
@@ -1,10 +1,12 @@
-eisvogel <- function(..., number_sections = TRUE,
+eisvogel <- function(..., toc = FALSE,
+		          number_sections = TRUE,
 		          toc_depth = 3,
 			  latex_engine = "xelatex",
 			  template = "eisvogel") {
   in_header <- system.file("includes", "in_header.tex",
                            package = "amaryaml")
   bookdown::pdf_document2(
+    toc = toc,
     number_sections = number_sections,
     toc_depth = toc_depth,
     latex_engine = latex_engine,
