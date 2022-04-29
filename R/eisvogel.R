39b858a4cda32ed5bd28fed814304d9c276e85a2
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-29 01:41:42 -0400

    added toc

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -1,4 +1,5 @@
 eisvogel <- function(..., number_sections = TRUE,
+			  toc = FALSE,
 		          toc_depth = 3,
 			  latex_engine = "xelatex",
 			  template = "eisvogel") {
@@ -6,6 +7,7 @@ eisvogel <- function(..., number_sections = TRUE,
                            package = "amaryaml")
   bookdown::pdf_document2(
     number_sections = number_sections,
+    toc = toc,
     toc_depth = toc_depth,
     latex_engine = latex_engine,
     template = template,
