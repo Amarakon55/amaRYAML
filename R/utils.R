3bb127fe2eb919a9194cfadf36090716d3c71df4
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-29 12:44:53 -0400

    fixed typo

--- R/utils.R
+++ R/utils.R
@@ -2,6 +2,6 @@ indent <- function(data) {
   knitr::kable(data, booktabs = TRUE, col.names = NULL) %>% 
   knitr::kable_styling(full_width = TRUE, position = "left") %>%
   kableExtra::column_spec(1, width = "5em" ) %>%
-  kableExtra::sub("\\\\toprule", "", .) %>%
-  kableExtra::sub("\\\\bottomrule", "", .)
+  sub("\\\\toprule", "", .) %>%
+  sub("\\\\bottomrule", "", .)
 }
