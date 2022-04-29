ad087a872f34a67f0ebf59cab47e05300e420694
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-29 14:12:07 -0400

    removed utils.R

--- R/utils.R
+++
@@ -1,7 +0,0 @@
-indent <- function(data) {
-  knitr::kable(data, booktabs = TRUE, col.names = NULL) %>% 
-  kableExtra::kable_styling(full_width = TRUE, position = "left") %>%
-  kableExtra::column_spec(1, width = "5em" ) %>%
-  sub("\\\\toprule", "", .) %>%
-  sub("\\\\bottomrule", "", .)
-}
