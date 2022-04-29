fd1851f57b5892e773bc9f0f0c4b667bb40aaed9
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-04-29 01:29:18 -0400

    removed toc

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -1,12 +1,10 @@
-eisvogel <- function(..., toc = FALSE,
-		          number_sections = TRUE,
+eisvogel <- function(..., number_sections = TRUE,
 		          toc_depth = 3,
 			  latex_engine = "xelatex",
 			  template = "eisvogel") {
   in_header <- system.file("includes", "in_header.tex",
                            package = "amaryaml")
   bookdown::pdf_document2(
-    toc = toc,
     number_sections = number_sections,
     toc_depth = toc_depth,
     latex_engine = latex_engine,
