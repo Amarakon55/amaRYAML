43df9183abf246e4f70a035b7e392ded7b06fc5a
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-05-01 01:26:52 -0400

    removed broken code

--- R/norbeam.R
+++ R/norbeam.R
@@ -1,9 +1,8 @@
 norbeam <- function(..., toc = FALSE,
 		         latex_engine = "xelatex",
 			 theme = "Nord",
-			 highlight = find_theme(norbeam, "theme.theme")) {
+			 highlight = find_theme(norbeam)) {
   in_header <- pkg_file("includes", "in_header.tex")
-  theme <- find_theme()
   bookdown::beamer_presentation2(
     latex_engine = latex_engine,
     theme = theme,
