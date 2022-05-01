f2b195f5bc990c97572e495affb445cf266b192b
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-05-01 01:25:41 -0400

    fixed highlight

--- R/norbeam.R
+++ R/norbeam.R
@@ -1,7 +1,7 @@
 norbeam <- function(..., toc = FALSE,
 		         latex_engine = "xelatex",
 			 theme = "Nord",
-			 highlight = find_theme(norbeam)) {
+			 highlight = find_theme(norbeam, "theme.theme")) {
   in_header <- pkg_file("includes", "in_header.tex")
   theme <- find_theme()
   bookdown::beamer_presentation2(
