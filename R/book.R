c636278f944038ce4a0018697a3e6faad232567f
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-05-03 22:39:49 -0400

    Returned latex engine to xelatex

--- R/book.R
+++ R/book.R
@@ -1,8 +1,10 @@
-book <- function(..., toc = FALSE,
+book <- function(..., latex_engine = "xelatex",
+		      toc = FALSE,
 		      toc_depth = 3) {
   in_header <- pkg_file("includes", "in_header.tex")
   indent <- pkg_file("includes", "indent.tex")
   bookdown::pdf_book(
+    latex_engine = latex_engine,
     toc = toc,
     toc_depth = toc_depth,
     includes = includes(in_header = in_header, before_body = indent), ...
