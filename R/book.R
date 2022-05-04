08b1eccfd0cd7613096bef0a0d8a7d3ce8c0b176
Amarakon55 <amarakon.al-zubaidi45@tutanota.com>
2022-05-03 21:01:03 -0400

    Changed font to Garamond

--- R/book.R
+++ R/book.R
@@ -1,12 +1,10 @@
 book <- function(..., toc = FALSE,
-		      toc_depth = 3,
-		      latex_engine = "xelatex") {
+		      toc_depth = 3) {
   in_header <- pkg_file("includes", "in_header.tex")
   indent <- pkg_file("includes", "indent.tex")
   bookdown::pdf_book(
     toc = toc,
     toc_depth = toc_depth,
-    latex_engine = latex_engine,
     includes = includes(in_header = in_header, before_body = indent), ...
   )
 }
