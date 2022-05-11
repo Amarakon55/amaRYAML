f17d01e57136242095f16d9439199b3c3dc7dcc0
amarakon <amar.al-zubaidi45@tutanota.com>
2022-05-11 17:24:41 -0400

    removed bookdown::

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -3,13 +3,11 @@ eisvogel <- function(..., number_sections = TRUE,
 		          toc_depth = 3,
 			  latex_engine = "xelatex",
 			  template = "eisvogel") {
-  # in_header <- pkg_file("includes", "in_header.tex")
   bookdown::pdf_document2(
     number_sections = number_sections,
     toc = toc,
     toc_depth = toc_depth,
     latex_engine = latex_engine,
     template = template, ...
-    #includes = includes(in_header = in_header), ...
   )
 }
