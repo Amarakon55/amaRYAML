52c26607dd2e7645c92972fdbe51ea40e5af35b3
amarakon <amar.al-zubaidi45@tutanota.com>
2022-05-23 14:53:56 -0400

    added resource

--- R/utils.R
+++ R/utils.R
@@ -2,6 +2,8 @@ pkg_file <- function(...) system.file(..., package = "amaryaml")
 
 pkg_file_template <- function(...) pkg_file("rmarkdown", "templates", ...)
 
+pkg_resource <- function(...) pkg_file("resources", mustWork = TRUE, ...)
+
 find_theme <- function(theme, file = "theme.theme") {
   res <- pkg_file_template(theme, "resources", file)
   if (res == "") {
