e39f8eb8cc40047415f8e976f5a5049e5bc5e8df
amarakon <amar.al-zubaidi45@tutanota.com>
2022-05-23 14:54:43 -0400

    fixed typo

--- R/github.R
+++ R/github.R
@@ -5,5 +5,5 @@ github <- function(...,
     bookdown::github_document2(toc = TRUE,
 			       number_sections = number_sections,
 			       includes = includes(before_body = before_body),
-			       .pandoc_args = c(lua_fiters("lower-header.lua")),
+			       .pandoc_args = c(lua_filters("lower-header.lua")),
 			       ...) }
