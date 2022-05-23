11b7f11670f65de83d76cbe4cf9d2329a4310b29
amarakon <amar.al-zubaidi45@tutanota.com>
2022-05-23 15:06:00 -0400

    fixed typo

--- R/github.R
+++ R/github.R
@@ -4,6 +4,6 @@ github <- function(...,
     before_body <- pkg_file("includes", "before_body.md")
     bookdown::github_document2(toc = TRUE,
 			       number_sections = number_sections,
+			       pandoc_args = c(lua_filters("lower-header.lua")),
 			       includes = includes(before_body = before_body),
-			       .pandoc_args = c(lua_filters("lower-header.lua")),
 			       ...) }
