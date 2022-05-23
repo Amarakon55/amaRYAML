fbc1e859e6f1fd80ee4aea2e97b2e598ab3fcfdd
amarakon <amar.al-zubaidi45@tutanota.com>
2022-05-23 14:11:57 -0400

    added lower-header.lua

--- R/github.R
+++ R/github.R
@@ -1,8 +1,10 @@
 github <- function(...,
 		   toc = TRUE,
-		   number_sections = FALSE) {
+		   number_sections = FALSE,
+		   pandoc_args = "--lua-filter = /home/amarakon/.local/bin/lower-header.lua") {
     before_body <- pkg_file("includes", "before_body.md")
     bookdown::github_document2(toc = TRUE,
 			       number_sections = number_sections,
+			       pandoc_args = pandoc_args,
 			       includes = includes(before_body = before_body),
 			       ...) }
