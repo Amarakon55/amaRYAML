8a221b9adeb78c1701201f487400bd59f4c7b83a
amarakon <amar.al-zubaidi45@tutanota.com>
2022-05-23 18:08:20 -0400

    removed setspace

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -1,7 +1,5 @@
 eisvogel <- function(..., number_sections = TRUE, toc = FALSE, toc_depth = 3, latex_engine = "xelatex",
   template = "eisvogel") {
-  in_header <- pkg_file("includes", "setspace.tex")
   bookdown::pdf_document2(number_sections = number_sections, toc = toc, toc_depth = toc_depth,
-    latex_engine = latex_engine, template = template, includes = includes(in_header = in_header),
-    ...)
+    latex_engine = latex_engine, template = template, ...)
 }
