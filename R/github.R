52c239cef2f8061b827ea0fc880c4e82fde21721
amarakon <amar.al-zubaidi45@tutanota.com>
2022-05-23 14:14:48 -0400

    fixed typo

--- R/github.R
+++ R/github.R
@@ -1,7 +1,7 @@
 github <- function(...,
 		   toc = TRUE,
 		   number_sections = FALSE,
-		   pandoc_args = "--lua-filter = /home/amarakon/.local/bin/lower-header.lua") {
+		   pandoc_args = "--lua-filter=/home/amarakon/.local/bin/lower-header.lua") {
     before_body <- pkg_file("includes", "before_body.md")
     bookdown::github_document2(toc = TRUE,
 			       number_sections = number_sections,
