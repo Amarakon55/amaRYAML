363079c431f430f25b3ccf388b9c359a83ccf18d
amarakon <amar.al-zubaidi45@tutanota.com>
2022-05-28 14:45:07 -0400

    tidied source

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -2,6 +2,6 @@ eisvogel <- function(..., number_sections = TRUE, toc = FALSE, toc_depth = 3, la
   template = "eisvogel") {
   tablist <- pkg_file("includes", "tablist.tex")
   bookdown::pdf_document2(number_sections = number_sections, toc = toc, toc_depth = toc_depth,
-    latex_engine = latex_engine, template = template,
-    includes = includes(in_header = tablist), ...)
+    latex_engine = latex_engine, template = template, includes = includes(in_header = tablist),
+    ...)
 }
