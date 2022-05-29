ff804cb1b65487527cb03921f02fead6cef26e94
amarakon <amar.al-zubaidi45@tutanota.com>
2022-05-28 22:37:11 -0400

    removed xelatex

--- R/book.R
+++ R/book.R
@@ -1,6 +1,6 @@
-book <- function(..., latex_engine = "xelatex", toc = FALSE, toc_depth = 3) {
+book <- function(..., toc = FALSE, toc_depth = 3) {
   tablist <- pkg_file("includes", "tablist.tex")
   indent <- pkg_file("includes", "indent.tex")
-  bookdown::pdf_book(latex_engine = latex_engine, toc = toc, toc_depth = toc_depth,
-    includes = includes(in_header = tablist, before_body = indent), ...)
+  bookdown::pdf_book(toc = toc, toc_depth = toc_depth, includes = includes(in_header = tablist,
+    before_body = indent), ...)
 }
--- R/norbeam.R
+++ R/norbeam.R
@@ -1,4 +1,4 @@
-norbeam <- function(..., toc = FALSE, latex_engine = "xelatex", theme = "Nord", highlight = find_theme("norbeam")) {
-  bookdown::beamer_presentation2(toc = toc, latex_engine = latex_engine, theme = theme,
-    highlight = highlight, ...)
+norbeam <- function(..., toc = FALSE, theme = "Nord", highlight = find_theme("norbeam")) {
+  bookdown::beamer_presentation2(toc = toc, theme = theme, highlight = highlight,
+    ...)
 }
