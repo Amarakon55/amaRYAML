daefcf3c4e0984da65b34afde45ae1b403656288
amarakon <amar.al-zubaidi45@tutanota.com>
2022-05-30 21:51:00 -0400

    fixed norbeam

--- R/norbeam.R
+++ R/norbeam.R
@@ -1,5 +1,4 @@
 norbeam <- function(..., toc = FALSE, theme = "Nord", highlight = find_theme("norbeam")) {
-  slideshow <- pkg_file("includes", "slideshow.tex")
-  bookdown::beamer_presentation2(toc = toc, theme = theme, highlight = highlight, includes = includes(in_header = slideshow),
+  bookdown::beamer_presentation2(toc = toc, theme = theme, highlight = highlight,
     ...)
 }
