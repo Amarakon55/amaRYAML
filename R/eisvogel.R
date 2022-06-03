0a7b8edd71ef65695cbe998c3a1c5d280109cdb3
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-03 13:08:26 -0400

    returned pandoc_args = NULL

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -1,5 +1,5 @@
 eisvogel <- function(..., colorlinks = TRUE, link_citations = TRUE, number_sections = TRUE,
-  toc = FALSE, toc_depth = 3, template = find_template("eisvogel")) {
+  toc = FALSE, toc_depth = 3, template = find_template("eisvogel"), pandoc_args = NULL) {
   tablist <- pkg_file("includes", "tablist.tex")
   pandoc_args <- c(pandoc_args, if (colorlinks) rmarkdown::pandoc_variable_arg("colorlinks"),
     if (link_citations) rmarkdown::pandoc_variable_arg("link-citations"))
