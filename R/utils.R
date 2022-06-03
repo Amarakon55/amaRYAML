d659cd7e6bd7a9a11ac38b62162489c0a8438b30
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-03 11:21:25 -0400

    added find_template function

--- R/utils.R
+++ R/utils.R
@@ -4,6 +4,14 @@ pkg_file_template <- function(...) pkg_file("rmarkdown", "templates", ...)
 
 pkg_resource <- function(...) pkg_file("resources", ..., mustWork = TRUE)
 
+find_template <- function(template, file = "template.tex") {
+  res <- pkg_file_template(template, "resources", file)
+  if (res == "") {
+    stop("Couldn't find template file ", template, "/resources/", file, call. = FALSE)
+  }
+  res
+}
+
 find_theme <- function(theme, file = "theme.theme") {
   res <- pkg_file_template(theme, "resources", file)
   if (res == "") {
