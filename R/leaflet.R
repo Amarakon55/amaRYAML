2e81985602bff2510e11c0b9f4eea1202590d859
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-03 13:16:56 -0400

    set colorlinks and link_citations to true

--- R/leaflet.R
+++ R/leaflet.R
@@ -1,5 +1,8 @@
-leaflet <- function(..., number_sections = FALSE, toc = FALSE) {
+leaflet <- function(..., colorlinks = TRUE, link_citations = TRUE, number_sections = FALSE,
+  toc = FALSE, pandoc_args = NULL) {
   leaflet <- pkg_file("includes", "leaflet.tex")
-  bookdown::pdf_document2(number_sections = number_sections, toc = toc,
-    includes = includes(in_header = leaflet), ...)
+  pandoc_args <- c(pandoc_args, if (colorlinks) rmarkdown::pandoc_variable_arg("colorlinks"),
+    if (link_citations) rmarkdown::pandoc_variable_arg("link-citations"))
+  bookdown::pdf_document2(number_sections = number_sections, toc = toc, includes = includes(in_header = leaflet),
+    pandoc_args = pandoc_args, ...)
 }
--- R/norbeam.R
+++ R/norbeam.R
@@ -1,4 +1,7 @@
-norbeam <- function(..., toc = FALSE, theme = "Nord", highlight = find_theme("norbeam")) {
+norbeam <- function(..., colorlinks = TRUE, link_citations = TRUE, toc = FALSE, theme = "Nord",
+  highlight = find_theme("norbeam"), pandoc_args = NULL) {
+  pandoc_args <- c(pandoc_args, if (colorlinks) rmarkdown::pandoc_variable_arg("colorlinks"),
+    if (link_citations) rmarkdown::pandoc_variable_arg("link-citations"))
   bookdown::beamer_presentation2(toc = toc, theme = theme, highlight = highlight,
-    ...)
+    pandoc_args = pandoc_args, ...)
 }
