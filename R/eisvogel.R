293c16caf8f3f651b8a51bb7467bd804ca67849c
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-03 11:17:42 -0400

    included template in package

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -1,4 +1,4 @@
-eisvogel <- function(..., number_sections = TRUE, toc = FALSE, toc_depth = 3, template = "eisvogel") {
+eisvogel <- function(..., number_sections = TRUE, toc = FALSE, toc_depth = 3, template = pkg_file_template("eisvogel", "resources", "template.tex")) {
   tablist <- pkg_file("includes", "tablist.tex")
   bookdown::pdf_document2(number_sections = number_sections, toc = toc, toc_depth = toc_depth,
     template = template, includes = includes(in_header = tablist), ...)
