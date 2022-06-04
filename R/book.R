c1981600b32ab6f8e59cbdb11858b65556d6f6a1
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-03 22:23:01 -0400

    returned book template back to normal

--- R/book.R
+++ R/book.R
@@ -1,10 +1,8 @@
-book <- function(..., colorlinks = TRUE, link_citations = TRUE, documentclass = "book", site = "bookdown::bookdown_site", toc = FALSE, toc_depth = 3, pandoc_args = NULL) {
+book <- function(..., colorlinks = TRUE, link_citations = TRUE, toc = FALSE, toc_depth = 3, pandoc_args = NULL) {
   tablist <- pkg_file("includes", "tablist.tex")
   indent <- pkg_file("includes", "indent.tex")
   pandoc_args <- c(pandoc_args, if (colorlinks) rmarkdown::pandoc_variable_arg("colorlinks"),
-    if (link_citations) rmarkdown::pandoc_variable_arg("link-citations"),
-    if (documentclass) rmarkdown::pandoc_variable_arg("documentclass"),
-    if (site) rmarkdown::pandoc_variable_arg("site"))
+    if (link_citations) rmarkdown::pandoc_variable_arg("link-citations"))
   bookdown::pdf_book(toc = toc, toc_depth = toc_depth, includes = includes(in_header = tablist,
     before_body = indent), pandoc_args = pandoc_args, ...)
 }
