022eb7fa9e5879cc2daa1365b7ffabc75ad2c33f
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-03 20:33:29 -0400

    added link colors

--- R/norbeam.R
+++ R/norbeam.R
@@ -1,7 +1,10 @@
-norbeam <- function(..., colorlinks = TRUE, link_citations = TRUE, toc = FALSE, theme = "Nord",
-  highlight = find_theme("norbeam"), pandoc_args = NULL) {
+norbeam <- function(..., colorlinks = TRUE, link_citations = TRUE, linkcolor = "NordRed",
+  filecolor = "NordRed", citecolor = "NordBlue", urlcolor = "NordBlue", toc = FALSE,
+  theme = "Nord", highlight = find_theme("norbeam"), pandoc_args = NULL) {
   pandoc_args <- c(pandoc_args, if (colorlinks) rmarkdown::pandoc_variable_arg("colorlinks"),
-    if (link_citations) rmarkdown::pandoc_variable_arg("link-citations"))
+    if (link_citations) rmarkdown::pandoc_variable_arg("link-citations"), if (linkcolor) rmarkdown::pandoc_variable_arg("linkcolor"),
+    if (filecolor) rmarkdown::pandoc_variable_arg("filecolor"), if (citecolor) rmarkdown::pandoc_variable_arg("citecolor"),
+    if (urlcolor) rmarkdown::pandoc_variable_arg("urlcolor"))
   bookdown::beamer_presentation2(toc = toc, theme = theme, highlight = highlight,
     pandoc_args = pandoc_args, ...)
 }
