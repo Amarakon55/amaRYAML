694bef7e5c30e08fc4bf396abeb3bcec89002436
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-03 22:14:00 -0400

    updated leaflet

--- R/leaflet.R
+++ R/leaflet.R
@@ -1,8 +1,9 @@
-leaflet <- function(..., colorlinks = TRUE, link_citations = TRUE, number_sections = FALSE,
+leaflet <- function(..., colorlinks = TRUE, link_citations = TRUE, links_as_notes = TRUE, number_sections = FALSE,
   toc = FALSE, pandoc_args = NULL) {
   leaflet <- pkg_file("includes", "leaflet.tex")
   pandoc_args <- c(pandoc_args, if (colorlinks) rmarkdown::pandoc_variable_arg("colorlinks"),
-    if (link_citations) rmarkdown::pandoc_variable_arg("link-citations"))
+    if (link_citations) rmarkdown::pandoc_variable_arg("link-citations"),
+    if (link_as_notes) rmarkdown::pandoc_variable_arg("links-as-notes"))
   bookdown::pdf_document2(number_sections = number_sections, toc = toc, includes = includes(in_header = leaflet),
     pandoc_args = pandoc_args, ...)
 }
