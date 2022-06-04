84b43c4849d8c6b4366e8402ea560078f887b389
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-03 21:11:23 -0400

    added norbeam template

--- R/norbeam.R
+++ R/norbeam.R
@@ -1,7 +1,7 @@
-norbeam <- function(..., colorlinks = TRUE, link_citations = TRUE, toc = FALSE, theme = "Nord",
+norbeam <- function(..., colorlinks = TRUE, link_citations = TRUE, toc = FALSE, template = find_template("norbeam"), theme = "Nord",
   highlight = find_theme("norbeam"), pandoc_args = NULL) {
   pandoc_args <- c(pandoc_args, if (colorlinks) rmarkdown::pandoc_variable_arg("colorlinks"),
     if (link_citations) rmarkdown::pandoc_variable_arg("link-citations"))
-  bookdown::beamer_presentation2(toc = toc, theme = theme, highlight = highlight,
+  bookdown::beamer_presentation2(toc = toc, template = template, theme = theme, highlight = highlight,
     pandoc_args = pandoc_args, ...)
 }
