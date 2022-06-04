d634dbd34a7c0543781dfb9c1448e1217c94e497
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-03 21:21:49 -0400

    added book template

--- R/book.R
+++ R/book.R
@@ -1,6 +1,6 @@
-book <- function(..., toc = FALSE, toc_depth = 3) {
+book <- function(..., toc = FALSE, toc_depth = 3, template = find_template("book")) {
   tablist <- pkg_file("includes", "tablist.tex")
   indent <- pkg_file("includes", "indent.tex")
-  bookdown::pdf_book(toc = toc, toc_depth = toc_depth, includes = includes(in_header = tablist,
+  bookdown::pdf_book(toc = toc, toc_depth = toc_depth, template = template, includes = includes(in_header = tablist,
     before_body = indent), ...)
 }
