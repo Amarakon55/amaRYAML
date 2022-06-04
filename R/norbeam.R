cb7035cbc6ba22f2e405881c55336ef2e02cd927
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-03 21:12:39 -0400

    tidied norbeam source code

--- R/norbeam.R
+++ R/norbeam.R
@@ -1,7 +1,7 @@
-norbeam <- function(..., colorlinks = TRUE, link_citations = TRUE, toc = FALSE, template = find_template("norbeam"), theme = "Nord",
-  highlight = find_theme("norbeam"), pandoc_args = NULL) {
+norbeam <- function(..., colorlinks = TRUE, link_citations = TRUE, toc = FALSE, template = find_template("norbeam"),
+  theme = "Nord", highlight = find_theme("norbeam"), pandoc_args = NULL) {
   pandoc_args <- c(pandoc_args, if (colorlinks) rmarkdown::pandoc_variable_arg("colorlinks"),
     if (link_citations) rmarkdown::pandoc_variable_arg("link-citations"))
-  bookdown::beamer_presentation2(toc = toc, template = template, theme = theme, highlight = highlight,
-    pandoc_args = pandoc_args, ...)
+  bookdown::beamer_presentation2(toc = toc, template = template, theme = theme,
+    highlight = highlight, pandoc_args = pandoc_args, ...)
 }
