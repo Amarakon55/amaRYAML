f69ea2b112b220ab68e8c62588057ba8d2c73a43
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-05 16:09:03 -0400

    tidied code

--- R/book.R
+++ R/book.R
@@ -1,7 +1,9 @@
-book <- function(..., link_citations = TRUE, toc = FALSE, toc_depth = 3, pandoc_args = NULL) {
+eisvogel <- function(..., link_citations = TRUE, number_sections = TRUE, toc = FALSE,
+  toc_depth = 3, template = find_template("eisvogel"), pandoc_args = NULL) {
   tablist <- pkg_file("includes", "tablist.tex")
-  indent <- pkg_file("includes", "indent.tex")
   pandoc_args <- c(pandoc_args, if (link_citations) rmarkdown::pandoc_variable_arg("link-citations"))
-  bookdown::pdf_book(toc = toc, toc_depth = toc_depth, includes = includes(in_header = tablist,
-    before_body = indent), pandoc_args = pandoc_args, ...)
+  bookdown::pdf_document2(number_sections = number_sections, toc = toc, toc_depth = toc_depth,
+    template = template, includes = includes(in_header = tablist), pandoc_args = pandoc_args,
+    ...)
 }
+
