4f6b28fa24dfb6deb089e593e4fb30de52530f38
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-05 00:19:28 -0400

    wrapped link-citations in quotes

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -3,7 +3,7 @@ eisvogel <- function(..., colorlinks = TRUE, link_citations = TRUE, number_secti
   tablist <- pkg_file("includes", "tablist.tex")
   pandoc_vars <- list()
   if (colorlinks) pandoc_vars$colorlinks <- TRUE
-  if (link_citations) pandoc_vars$link-citations <- TRUE
+  if (link_citations) pandoc_vars$"link-citations" <- TRUE
   document2(number_sections = number_sections, toc = toc, toc_depth = toc_depth,
     template = template, includes = includes(in_header = tablist), pandoc_vars = pandoc_vars,
     ...)
