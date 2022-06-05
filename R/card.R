d0233cda72d8e02c7f6a31f9443a3adaaeddd4ce
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-05 18:27:30 -0400

    added card template

---
+++ R/card.R
@@ -0,0 +1,6 @@
+card <- function(..., colorlinks = TRUE, links_as_notes = TRUE, template = find_template("card"),
+  pandoc_args = NULL) {
+  pandoc_args <- c(pandoc_args, if (colorlinks) rmarkdown::pandoc_variable_arg("colorlinks"),
+    if (links_as_notes) rmarkdown::pandoc_variable_arg("links-as-notes"))
+  rmarkdown::pdf_document(template = template, pandoc_args = pandoc_args, ...)
+}
