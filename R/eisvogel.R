5b35c89b8cdb65acca0119a4bfa590698cfada61
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-11 00:03:20 -0400

    improved URLs

--- R/eisvogel.R
+++ R/eisvogel.R
@@ -1,7 +1,9 @@
-eisvogel <- function(..., link_citations = TRUE, number_sections = TRUE,
+#' @export
+eisvogel <- function(..., colorlinks = TRUE, link_citations = TRUE, number_sections = TRUE,
   toc = FALSE, toc_depth = 3, template = find_template("eisvogel"), pandoc_args = NULL) {
   tablist <- pkg_file("includes", "tablist.tex")
-  pandoc_args <- c(pandoc_args, if (link_citations) rmarkdown::pandoc_variable_arg("link-citations"))
+  pandoc_args <- c(pandoc_args, if(colorlinks) rmarkdown::pandoc_variable_arg("colorlinks"),
+		   if (link_citations) rmarkdown::pandoc_variable_arg("link-citations"))
   bookdown::pdf_document2(number_sections = number_sections, toc = toc, toc_depth = toc_depth,
     template = template, includes = includes(in_header = tablist), pandoc_args = pandoc_args,
     ...)
