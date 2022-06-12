be23f5497ef0a2366ab7cfb6b61f1a06af104c0d
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-12 19:57:11 -0400

    set default options

--- R/table.R
+++ R/table.R
@@ -1,9 +1,9 @@
 #' @export
-kabularx <- function(...) knitr::kable(escape = FALSE, booktabs = TRUE, tabularx = TRUE,
-  linesep = "\\addlinespace", ...) |>
+kabularx <- function(..., escape = FALSE, booktabs = TRUE, linesep = "\\addlinespace") knitr::kable(tabularx = TRUE,
+  escape = escape, booktabs = booktabs, linesep = linesep, ...) |>
   kableExtra::kable_styling(latex_options = "hold_position")
 
 #' @export
-xlkabular <- function(...) knitr::kable(escape = FALSE, booktabs = TRUE, xltabular = TRUE,
-  linesep = "\\addlinespace", ...) |>
+xlkabular <- function(..., escape = FALSE, booktabs = TRUE, linesep = "\\addlinespace") knitr::kable(xltabular = TRUE,
+  escape = escape, booktabs = booktabs, linesep = linesep, ...) |>
   kableExtra::kable_styling(latex_options = "repeat_header")
