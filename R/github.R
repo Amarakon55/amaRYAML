145f21b72616d1f53160b0f00ec9b36c04fca985
amarakon <amar.al-zubaidi45@tutanota.com>
2022-06-13 13:38:56 -0400

    removed unneeded lua resource

--- R/github.R
+++ R/github.R
@@ -1,6 +1,6 @@
 #' @export
 github <- function(..., toc = TRUE, number_sections = FALSE) {
   before_body <- pkg_file("includes", "before_body.md")
-  bookdown::github_document2(toc = TRUE, number_sections = number_sections, pandoc_args = c(lua_filters("lower-header.lua")),
+  bookdown::github_document2(toc = TRUE, number_sections = number_sections, pandoc_args = "--shift-heading-level-by=1",
     includes = rmarkdown::includes(before_body = before_body), ...)
 }
