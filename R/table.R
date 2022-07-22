dc2b0a8ec568cf26810efa792d372a033065b76a
amarakon <amar.al-zubaidi45@tutanota.com>
2022-07-22 05:38:56 -0400

    Update table functions

--- R/table.R
+++ R/table.R
@@ -1,9 +1,9 @@
 #' @export
-kabularx <- function(..., escape = FALSE, booktabs = TRUE, linesep = "\\addlinespace") knitr::kable(tabularx = TRUE,
+kabularx <- function(..., escape = FALSE, booktabs = TRUE, linesep = "\\addlinespace") knitr::kable(tabular = "tabularx",
   escape = escape, booktabs = booktabs, linesep = linesep, ...) |>
   kableExtra::kable_styling(latex_options = "hold_position")
 
 #' @export
-xlkabular <- function(..., escape = FALSE, booktabs = TRUE, linesep = "\\addlinespace") knitr::kable(xltabular = TRUE,
+xlkabular <- function(..., escape = FALSE, booktabs = TRUE, linesep = "\\addlinespace") knitr::kable(tabular = "xltabular",
   escape = escape, booktabs = booktabs, linesep = linesep, ...) |>
   kableExtra::kable_styling(latex_options = "repeat_header")
